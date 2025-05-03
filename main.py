6ca69cd953ed2237f9c4d9177c7fd2fc33bcacff
zainirejab <zainirejab@gmail.com>
2025-05-03 22:40:21 +0300

    Initial commit fastapi

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import UUID, uuid4
+
+app = FastAPI()
+
+class Task(BaseModel):
+  id: Optional[UUID] = None
+  title: str
+  description: Optional[str] = None
+  completed: bool = False
+  
+tasks = []
+
+@app.post("/tasks/", response_model=Task)
+def create_task(task: Task):
+  task.id = uuid4()
+  tasks.append(task)
+  return task
+
+@app.get("/tasks/", response_model=List[Task])
+def read_tasks():
+  return tasks
+
+#view specific task
+
+
+if __name__ == "__main__":
+  import uvicorn
+  uvicorn.run(app, host="0.0.0.0", port=8000)
